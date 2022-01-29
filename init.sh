9777d0776dcd64d03004e9d59754739400bff963
Pavel Syrnev <groupozzy@gmail.com>
2022-01-29 15:42:45 +0700

    init.sh with gunicorn

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@
 sudo ln -sf /home/box/web/etc/nginx80.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
+sudo gunicorn -c /etc/gunicorn.d/test.conf.py
