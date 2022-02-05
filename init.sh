288f2fc00188c4ba4eac7d68d33ea8d07800fc23
Pavel Syrnev <groupozzy@gmail.com>
2022-02-05 19:21:54 +0700

    lesson 2.1 init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-﻿#sudo /etc/init.d/mysql start﻿
+#sudo /etc/init.d/mysql start
 sudo ln -sf /home/box/web/etc/nginx80.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
 sudo ln -s /home/box/web/etc/dj_gunicorn.conf.py   /etc/gunicorn.d/dj_test.conf.py
 
-sudo gunicorn -c /usr/local/var/www/stepik/etc/gunicorn.conf.py hello:wsg_application --daemon
-sudo gunicorn -c /usr/local/var/www/stepik/etc/dj_gunicorn.conf.py ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:wsg_application --daemon
+sudo gunicorn -c /home/box/web/etc/dj_gunicorn.conf.py ask.wsgi:application
