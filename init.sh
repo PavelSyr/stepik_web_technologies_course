648fdd3912002e0e3637672624dd39bc78b51d2d
Pavel Syrnev <groupozzy@gmail.com>
2022-02-05 19:48:01 +0700

    lesson 2.1 init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@
 sudo ln -sf /home/box/web/etc/nginx80.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
-sudo ln -sf /home/box/web/etc/dj_gunicorn.conf.py   /etc/gunicorn.d/dj_test.conf.py
+#sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.conf.py
+#sudo ln -sf /home/box/web/etc/dj_gunicorn.conf.py   /etc/gunicorn.d/dj_test.conf.py
 
-sudo gunicorn -c /etc/gunicorn.d/test.conf.py hello:wsg_application --daemon
-sudo gunicorn -c /etc/gunicorn.d/dj_test.conf.py ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:wsg_application --daemon
+sudo gunicorn -c /home/box/web/etc/dj_gunicorn.conf.py ask.wsgi:application
